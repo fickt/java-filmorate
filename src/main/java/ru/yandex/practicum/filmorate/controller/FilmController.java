e989db1f14f3adaa22b9f131023e2c3e28f09bad
Michael Arbuzov <garrys2machinima@gmail.com>
2022-06-13 23:02:48 +0700

    3-d edition

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,7 +1,5 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import lombok.extern.slf4j.Slf4j;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,25 +12,24 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 
-@Slf4j
 @RestController
 @RequestMapping("/films")
 public class FilmController {
 
     private FilmStorage filmStorage = new FilmStorage();
     private int filmIdGenerator = 0;
-
+    private Logger filmControllerLogger = Logger.getLogger("filmControllerLogger");
 
     @PostMapping("/add")
     public Film addFilm(@RequestBody Film film) {
         if (film.getName() == null || film.getName().isBlank() || film.getDescription().length() >= 200 ||
                 film.getReleaseDate().isBefore(LocalDate.of(1895, Month.DECEMBER, 28))
                 || film.getDuration().isNegative()) {
-            Logger.getLogger("logger").log(Level.WARNING, "ValidationException /film/add");
+            filmControllerLogger.log(Level.WARNING, "ValidationException /film/add");
             throw new ValidationException("Ошибка валидации /film/add");
 
         } else {
-            Logger.getLogger("logger").log(Level.INFO, "film has been added /add");
+            filmControllerLogger.log(Level.INFO, "film has been added /add");
             film.setId(filmIdGenerator);
             filmStorage.addFilm(filmIdGenerator, film);
             filmIdGenerator++;
@@ -44,10 +41,10 @@ public class FilmController {
     public Film updateFilm(@RequestBody Film film) {
         if (filmStorage.containsFilm(film.getId())) {
             filmStorage.addFilm(film.getId(), film);
-            Logger.getLogger("logger").log(Level.INFO, "film has been updated /update");
+            filmControllerLogger.log(Level.INFO, "film has been updated /update");
             return film;
         } else {
-            Logger.getLogger("logger").log(Level.WARNING, "ValidationException /film/update");
+            filmControllerLogger.log(Level.WARNING, "ValidationException /film/update");
             throw new ValidationException("Ошибка валидации /film/update");
         }
     }
@@ -55,7 +52,7 @@ public class FilmController {
     @ResponseBody
     @GetMapping("/allfilms")
     public ResponseEntity getAllFilms() {
-        Logger.getLogger("logger").log(Level.INFO, " /allfilms");
+        filmControllerLogger.log(Level.INFO, " /allfilms");
         return new ResponseEntity(filmStorage.getAllFilms().values(), HttpStatus.OK);
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,20 +11,20 @@ import java.time.LocalDate;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-@Slf4j
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
     private UserStorage userStorage = new UserStorage();
     private int userIdGenerator = 0;
-
+    private Logger userControllerLogger = Logger.getLogger("userControllerLogger");
     @PostMapping("/newuser")
     public User createNewUser(@RequestBody User user) {
         if (user.getEmail() == null || user.getEmail().isBlank() || !user.getEmail().contains("@") ||
                 user.getLogin().isBlank() ||
                 user.getLogin().contains(" ") || user.getBirthday().isAfter(LocalDate.now())) {
-            Logger.getLogger("logger").log(Level.WARNING, "ValidationException /newuser");
+            userControllerLogger.log(Level.WARNING, "ValidationException /newuser");
             throw new ValidationException("Ошибка валидации /user/newuser");
         } else {
             if (user.getName().isBlank()) {
@@ -33,7 +32,7 @@ public class UserController {
             }
             userStorage.addUser(userIdGenerator, user);
             userIdGenerator++;
-            Logger.getLogger("logger").log(Level.INFO, "user has been created! /newuser");
+            userControllerLogger.log(Level.INFO, "user has been created! /newuser");
         }
         return user;
     }
@@ -42,7 +41,7 @@ public class UserController {
     public User updateUser(@RequestBody User user) {
         if (userStorage.containsUser(user.getId())) {
             userStorage.addUser(user.getId(), user);
-            Logger.getLogger("logger").log(Level.INFO, "user has been updated! /updateuser");
+            userControllerLogger.log(Level.INFO, "user has been updated! /updateuser");
             return user;
         } else {
             Logger.getLogger("logger").log(Level.WARNING, "ValidationException /updateuser");
@@ -53,7 +52,7 @@ public class UserController {
     @GetMapping("/allusers")
     @ResponseBody
     public ResponseEntity getAllUsers() {
-        Logger.getLogger("logger").log(Level.INFO, " /allusers");
+        userControllerLogger.log(Level.INFO, " /allusers");
         return new ResponseEntity(userStorage.getAllUsers().values(), HttpStatus.OK);
     }
 }
